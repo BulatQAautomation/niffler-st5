e48f44011962c9d2d0cc3bb31e43c82821d9dfaa
bulatkiyam <kiyam92@gmail.com>
2025-11-10 21:25:30 +0300

    еще правки

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/ContextExtension.java
@@ -0,0 +1,8 @@
+package guru.qa.niffler.jupiter.extension;
+
+import org.junit.jupiter.api.extension.Extension;
+
+public class ContextExtension implements Extension {
+    // Базовое расширение JUnit 5
+}
+
